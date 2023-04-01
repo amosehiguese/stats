ee1fb42bc09425e9810bea167b0552c985b458ea
amosehiguese <amosehiguese@gmail.com>
2023-04-01 11:02:25 +0100

    Modify TestMean

--- descriptive/mean_test.go
+++ descriptive/mean_test.go
@@ -2,12 +2,23 @@ package descriptive
 
 import "testing"
 
+const (
+    checkMark = "\u2713"
+    ballotX = "\u2717"
+)
+
 func TestMean(t *testing.T) {
     data := []float64{1.0, 3.0, 5.0, 7.0}
-    expected := 3.0
+    expected := 4.0
     result := Mean(data)
-    if result != expected {
-        t.Errorf("Mean of %v was incorrect, got: %v, want: %v.", data, result, expected)
+
+    t.Log("\t\tGiven the need to test Mean.")
+    {
+
+        if result != expected {
+            t.Fatalf("Mean of %v was incorrect, got: %v, want: %v. %v", data, result, expected, ballotX)
+        }
+        t.Log("\t\tShould be able to get expected mean.", checkMark)
     }
 }
 
