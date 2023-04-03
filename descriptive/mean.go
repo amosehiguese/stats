17d72bcdb3a737a8c7e7450392362f3e633072ed
amosehiguese <amosehiguese@gmail.com>
2023-04-03 09:22:24 +0100

    Modify mean.go

--- descriptive/mean.go
+++ descriptive/mean.go
@@ -1,10 +1,18 @@
 package descriptive
 
+import (
+	"fmt"
+)
+
 // Mean calculates the mean of a slice of float64 values.
-func Mean(data []float64) float64 {
+func Mean(data []float64) (float64, error) {
+    if len(data) == 0 {
+        err := fmt.Errorf("empty slice not allowed")
+        return 0.0, err
+    }
     var sum float64
     for _, x := range data {
         sum += x
     }
-    return sum / float64(len(data))
+    return sum / float64(len(data)), nil
 }
